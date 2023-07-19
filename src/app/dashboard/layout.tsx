21920405ea8073e0deb5d053639032ff965b9ff2
Jezer Rázuri <jezerrazuri@upeu.edu.pe>
2023-07-19 14:34:07 -0500

    first commit

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import './dashboard.css';
-import SideBar from '@/components/SideBar';
+import "./dashboard.css";
+import SideBar from "@/components/SideBar";
 
-import { BellIcon, QuestionMarkCircleIcon } from '@heroicons/react/24/solid';
+import { BellIcon, QuestionMarkCircleIcon } from "@heroicons/react/24/solid";
 
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { GlobeAltIcon } from "@heroicons/react/24/outline";
 
 export default function DashboardLayout({
   children,
@@ -13,26 +13,26 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="h-screen flex items-stretch">
+    <div className="flex h-screen items-stretch">
       <SideBar />
-      <main className=" w-full h-full p-4 flex flex-col gap-4 relative overflow-y-auto bg-[#EEE]">
-        <div className=" w-full h-full absolute inset-0 -z-10" />
+      <main className="relative flex h-full w-full flex-col gap-4 overflow-y-auto bg-[#EEE] p-4">
+        <div className="absolute inset-0 -z-10 h-full w-full" />
         <header className="flex items-center justify-between">
           <div className="text-[#757575]">
-            <div className="font-bold text-2xl">Inicio</div>
+            <div className="text-2xl font-bold">Inicio</div>
             <div className="text-sm">
               Bienvenido, <span className="font-bold">David Reyna</span>
             </div>
           </div>
-          <div className="bg-[#FF9853] rounded-lg flex gap-3 text-white px-4 py-3">
-            <button className="w-5 h-5">
+          <div className="flex gap-3 rounded-lg bg-[#FF9853] px-4 py-3 text-white">
+            <button className="h-5 w-5">
               <QuestionMarkCircleIcon />
             </button>
-            <button className="w-5 h-5 relative">
-              <div className="w-2 h-2 bg-red-600 absolute rounded-full right-0 top-0"></div>
+            <button className="relative h-5 w-5">
+              <div className="absolute right-0 top-0 h-2 w-2 rounded-full bg-red-600"></div>
               <BellIcon />
             </button>
-            <button className="w-5 h-5">
+            <button className="h-5 w-5">
               <GlobeAltIcon />
             </button>
           </div>
