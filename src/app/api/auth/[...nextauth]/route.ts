2b640e6f7fe4f8f0bc60946f0a11c6155e7679ca
Christian Atencio <48265256+ChristianAP@users.noreply.github.com>
2023-10-04 18:12:35 -0500

    (chore auth)

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,49 @@
+import { LoginAuthService } from "@/services";
+import { log } from "console";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+// const loginService = new LoginDatasourseImp();
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        USR_USERNAME: {
+          label: "Username",
+          type: "text",
+          placeholder: "Username",
+        },
+        USR_PASSWORD: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {        
+
+        const login = await LoginAuthService({
+          username: req.body!.username,
+          password: req.body!.password,
+        });
+
+        if (login.status !== 200) {
+            throw new Error(`${login.message}`)
+        }
+        return login;
+      },
+    }),
+  ],
+
+  callbacks: {
+    jwt({ token, user, account }) {
+      if (user) token.user = user;
+      return token;
+    },
+    session({ session, token }) {
+      if (token) session.user = token.user as any;
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/",
+  },
+});
+
+export { handler as GET, handler as POST };
